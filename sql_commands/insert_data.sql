3f4e7bb66ec825939e407677e283b6346c9b7bb0
Manohar Bandam <mb@a4-aado-am1883.ad.psu.edu>
2021-12-09 05:06:52 -0500

    reference tables data

--- sql_commands/insert_data.sql
+++ sql_commands/insert_data.sql
@@ -1,89 +1,91 @@
 -- Students Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/students.csv'
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/students.csv'
  INTO TABLE ref_students
  FIELDS TERMINATED BY ','
  LINES TERMINATED BY '\r\n'
-   IGNORE 1 LINES
- (fname,mname,lname,email_id);
+ (fname,mname,lname,email_id,password,last_logged_in,country_code,phone_number);
 
 --SET FOREIGN_KEY_CHECKS=0;
 --SET FOREIGN_KEY_CHECKS=1;
 -- Instructors Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/instructors.csv'
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/instructors.csv'
  INTO TABLE ref_instructors
  FIELDS TERMINATED BY ','
  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES
- (instructor_id,fname,mname,lname,email_id);
+ (fname,mname,lname,email_id,password);
+
+-- ref_department Table load
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/departments.csv'
+ INTO TABLE ref_department
+ FIELDS TERMINATED BY ','
+ LINES TERMINATED BY '\n'
+ (dept_name);
 
 -- ref_courses Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/courses.csv'
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/courses.csv'
  INTO TABLE ref_courses
  FIELDS TERMINATED BY ','
  LINES TERMINATED BY '\n'
-   IGNORE 1 LINES
- (courses_id,name,stream);
+ (course_name,dept_id);
 
 -- ref_room Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/rooms.csv'
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/rooms.csv'
  INTO TABLE ref_room
  FIELDS TERMINATED BY ','
  LINES TERMINATED BY '\r\n'
-    IGNORE 1 LINES
- (room_id,room_code,capacity);
+ (room_name,capacity);
 
 -- Students Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/schedules.csv'
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/schedules.csv'
  INTO TABLE ref_schedules
  FIELDS TERMINATED BY ','
  LINES TERMINATED BY '\r\n'
-    IGNORE 1 LINES
- (schedule_id,start_time,end_time,occurrence);
+ (occurrence,start_time,end_time);
 
--- ref_term Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/ref_term.csv'
- INTO TABLE ref_term
- FIELDS TERMINATED BY ','
- LINES TERMINATED BY '\r\n'
- (fld1,fld2,fld3,fld4);
+   --course_offerings table
+   LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/course_offerings.csv'
+    INTO TABLE course_offerings
+    FIELDS TERMINATED BY ','
+    LINES TERMINATED BY '\n'
+    (course_uuid,instructor_id,room_id,schedule_id);
 
 -- courses_cart Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/courses_cart.csv'
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/courses_cart.csv'
  INTO TABLE courses_cart
  FIELDS TERMINATED BY ','
  LINES TERMINATED BY '\r\n'
  (fld1,fld2,fld3,fld4);
 
 -- courses_taken Table load
-LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/courses_taken.csv'
+LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/courses_taken.csv'
  INTO TABLE courses_taken
  FIELDS TERMINATED BY ','
  LINES TERMINATED BY '\r\n'
  (fld1,fld2,fld3,fld4);
 
+
+
+
+
+
+
+
+
  --sections table
- LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/sections.csv'
+ LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/sections.csv'
   INTO TABLE sections
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
-     IGNORE 1 LINES
   (uuid,course_offering_uuid,section_type,number,room_uuid,schedule_uuid);
 
   --teachings table
-  LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/teachings.csv'
+  LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/teachings.csv'
    INTO TABLE teachings
    FIELDS TERMINATED BY ','
    LINES TERMINATED BY '\n'
-      IGNORE 1 LINES
    (instructor_id,section_uuid);
 
-  --course_offerings table
-  LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/course_offerings.csv'
-   INTO TABLE course_offerings_tmp
-   FIELDS TERMINATED BY ','
-   LINES TERMINATED BY '\n'
-      IGNORE 1 LINES
-   (uuid,course_uuid,term_code,name);
+
 
 
  Insert into course_offerings
@@ -92,3 +94,10 @@ LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data/courses_taken.csv'
  where a.course_uuid = d.courses_id
  and b.section_uuid = c.uuid
  and a.uuid = c.course_offering_uuid;
+
+ -- ref_term Table load
+ LOAD DATA LOCAL INFILE '/home/mmb7103/DBMS_431W/data_new/ref_term.csv'
+  INTO TABLE ref_term
+  FIELDS TERMINATED BY ','
+  LINES TERMINATED BY '\r\n'
+  (fld1,fld2,fld3,fld4);
