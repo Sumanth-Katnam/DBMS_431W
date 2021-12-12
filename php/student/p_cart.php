82845ab73fadd7a792d12c1d7c8701466a31dc08
Manohar Bandam <mb@a4-aado-am1883.ad.psu.edu>
2021-12-12 17:10:39 -0500

    status and availability

--- php/student/p_cart.php
+++ php/student/p_cart.php
@@ -12,21 +12,43 @@
         include '../../models/CourseCartEntry.php';
 
         $id = $_SESSION['user_id'];
-        $select_query=  "SELECT CE.cart_entry_id, C.course_name, 
-            CONCAT(I.fname, ' ', I.mname, ' ', I.lname) AS instructor_name, 
-            S.occurrence, CONCAT(S.start_time, ' - ', S.end_time) AS schedule_time, R.room_name
-                FROM courses_cart_entry CE 
-                JOIN course_offerings CO 
-                ON CE.offering_id = CO.offering_id
-                JOIN ref_courses C
-                ON CO.course_id = C.course_id
-                JOIN ref_instructors I
-                ON CO.instructor_id = I.instructor_id
-                JOIN ref_schedules S
-                ON CO.schedule_id = S.schedule_id
-                JOIN ref_room R
-                ON CO.room_id = R.room_id
-                WHERE CE.student_id = '$id';";
+        $select_query=  "SELECT BASE.cart_entry_id,BASE.course_name,BASE.instructor_name,BASE.schedule_time,BASE.room_name,
+                           CASE
+                             WHEN BASE.schedule_id IN(
+                                     SELECT CO.schedule_id
+                                     FROM courses_cart_entry CE, course_offerings CO
+                                     WHERE CE.offering_id = CO.offering_id
+                                     AND CE.student_id = '.$id.'
+                                     GROUP BY CO.schedule_id
+                                     HAVING count(*)>1) THEN 'CONFLICT'
+                             WHEN BASE.availablity = 0 THEN 'FILLED'
+                             ELSE BASE.availablity
+                           END AS status
+                             FROM (SELECT CE.cart_entry_id, C.course_name,
+                                 CONCAT(I.fname, ' ', I.mname, ' ', I.lname) AS instructor_name,
+                                 S.occurrence, CONCAT(S.start_time, ' - ', S.end_time) AS schedule_time, R.room_name, a3.availablity, S.schedule_id
+                                   FROM courses_cart_entry CE
+                                   JOIN course_offerings CO
+                                   ON CE.offering_id = CO.offering_id
+                                   JOIN ref_courses C
+                                   ON CO.course_id = C.course_id
+                                   JOIN ref_instructors I
+                                   ON CO.instructor_id = I.instructor_id
+                                   JOIN ref_schedules S
+                                   ON CO.schedule_id = S.schedule_id
+                                   JOIN ref_room R
+                                   ON CO.room_id = R.room_id
+                                   JOIN (SELECT a1.offering_id, (a2.capacity - a1.filled) as availablity
+                                         FROM ( SELECT CT.offering_id,count(*) AS filled
+                                               FROM course_offerings CO, courses_taken CT
+                                               WHERE CO.offering_id = CT.offering_id
+                                               GROUP BY CT.offering_id) a1
+                                         JOIN (SELECT r.capacity, c.offering_id
+                                               FROM ref_room r, course_offerings c
+                                               WHERE r.room_id = c.room_id) a2
+                                         ON a1.offering_id = a2.offering_id) a3
+                                   ON CE.offering_id = a3.offering_id
+                                   WHERE CE.student_id = $id) BASE;";
 
         if(!$cartEntries = mysqli_query($con, $select_query)) {
             echo "Failed to retrieve Cart Entries";
@@ -35,7 +57,7 @@
             while($entry = mysqli_fetch_assoc($cartEntries)){
                 $cartObj = new CourseCartEntry($entry['cart_entry_id'], $entry['course_name'], 
                     $entry['instructor_name'], $entry['occurrence'], 
-                    $entry['schedule_time'], $entry['room_name'], '');
+                    $entry['schedule_time'], $entry['room_name'], $entry['status']);
                 array_push($entriesArr, $cartObj);
             }
             echo json_encode($entriesArr);
